985713ebc73aa1dee1b2ed76ae6f6200c209732c
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-16 16:09:01 +0300

    add upload profile function

--- insta/views.py
+++ insta/views.py
@@ -65,4 +65,49 @@ def like(request,pic_id):
 	Pic = Pic.objects.get(id=pic_id)
 	like +=1
 	save_like()
-	return redirect(timeline)        
+	return redirect(timeline)   
+
+
+def search_results(request):
+    if 'pic' in request.GET and request.GET["pic"]:
+        search_term = request.GET.get("pic")
+        searched_profiles = Profile.search_profile(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search_pic.html',{"message":message,"pics": searched_profiles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search_pic.html',{"message":message})
+
+
+@login_required(login_url='/accounts/login/')
+def upload_profile(request):
+    current_user = request.user 
+    title = 'Upload Profile'
+    try:
+        requested_profile = Profile.objects.get(user_id = current_user.id)
+        if request.method == 'POST':
+            form = ProfileUploadForm(request.POST,request.FILES)
+
+            if form.is_valid():
+                requested_profile.profile_pic = form.cleaned_data['profile_pic']
+                requested_profile.bio = form.cleaned_data['bio']
+                requested_profile.username = form.cleaned_data['username']
+                requested_profile.save_profile()
+                return redirect( profile )
+        else:
+            form = ProfileUploadForm()
+    except:
+        if request.method == 'POST':
+            form = ProfileUploadForm(request.POST,request.FILES)
+
+            if form.is_valid():
+                new_profile = Profile(profile_pic = form.cleaned_data['profile_pic'],bio = form.cleaned_data['bio'],username = form.cleaned_data['username'])
+                new_profile.save_profile()
+                return redirect( profile )
+        else:
+            form = ProfileUploadForm()
+
+
+    return render(request,'upload_profile.html',{"title":title,"current_user":current_user,"form":form})         
