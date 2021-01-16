c263854ec24a6f88bd8844afa3ceb4474fb43318
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-16 16:02:13 +0300

    add comment function

--- insta/views.py
+++ insta/views.py
@@ -15,3 +15,24 @@ def index(request):
 
       print(pic_posts)
       return render(request, 'index.html', {"title":title,"pic_posts":pic_posts})
+
+@login_required(login_url='/accounts/login/')
+def comment(request,id):
+	
+	post = get_object_or_404(Pic,id=id)	
+	current_user = request.user
+	print(post)
+
+	if request.method == 'POST':
+		form = CommentForm(request.POST)
+
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.user = current_user
+			comment.pic = post
+			comment.save()
+			return redirect('index')
+	else:
+		form = CommentForm()
+
+	return render(request,'comment.html',{"form":form})        
