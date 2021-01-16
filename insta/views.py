a4d2c8ede992bf1f7e37b6d5a7f2378b3edbb107
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-16 16:00:28 +0300

    add index function in views

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from . forms import ProfileUploadForm,CommentForm,ProfileForm
+from django.http  import HttpResponse
+from . models import Pic ,Profile, Likes, Follow, Comment,Unfollow
+from django.conf import settings
+
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+      title = 'Instagram'
+      pic_posts = Pic.objects.all()
+      # comments = Comment.objects.all()
+
+      print(pic_posts)
+      return render(request, 'index.html', {"title":title,"pic_posts":pic_posts})
