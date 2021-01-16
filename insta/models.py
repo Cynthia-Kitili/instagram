67ea73a7d5d011a395eeb0e1cb05f28d330c3cf6
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-16 15:15:18 +0300

    add Picture model in models

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Pic(models.Model):
+    pic = models.ImageField(upload_to = "pics/",null = True)
+    user = models.ForeignKey(User,null=True)
+    pic_name = models.CharField(max_length = 30,null = True)
+    likes = models.IntegerField(default=0)
+    pic_caption = models.TextField(null = True)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+    # profile = models.ForeignKey(Profile, null=True) 
+    comments = models.IntegerField(default=0)
+
+
+    def __str__(self):
+    	return self.pic_name
