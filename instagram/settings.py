e96c0c7701b457d39528dd6d3f08341a748843a3
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-19 11:56:17 +0300

    deployment to Heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g6n^xgsxqzx-2%__@m)p51&yj68v&r4y-d1$byj-rowf%!^#mr'
+# SECRET_KEY = 'g6n^xgsxqzx-2%__@m)p51&yj68v&r4y-d1$byj-rowf%!^#mr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
